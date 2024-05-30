69109da4edbd66766d15a72024677eb537e17d58
Thomas <ThomasGrahamBiz@aol.com>
2024-05-30 14:35:35 -0400

    Added HTTP Methods..

--- index.js
+++ index.js
@@ -17,10 +17,77 @@ const pool = new Pool({
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Returns array of flavors
 app.get("/api/flavors", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM flavors ORDER BY id");
     res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+});
+
+// Returns a single flavor
+app.get("/api/flavors/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("SELECT * FROM flavors WHERE id = $1", [
+      id,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Flavor not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+});
+
+// Creates a new flavor
+app.post("/api/flavors", async (req, res) => {
+  try {
+    const { name, is_favorite } = req.body;
+    const result = await pool.query(
+      "INSERT INTO flavors (name, is_favorite) VALUES ($1, $2) RETURNING *",
+      [name, is_favorite]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+});
+
+// Deletes a flavor
+app.delete("/api/flavors/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("DELETE FROM flavors WHERE id = $1", [id]);
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Flavor not found" });
+    }
+    res.sendStatus(204);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+});
+
+// Updates a flavor
+app.put("/api/flavors/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, is_favorite } = req.body;
+    const result = await pool.query(
+      "UPDATE flavors SET name = $1, is_favorite = $2, updated_at = CURRENT_TIMESTAMP WHERE id = $3 RETURNING *",
+      [name, is_favorite, id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Flavor not found" });
+    }
+    res.json(result.rows[0]);
   } catch (err) {
     console.error(err.message);
     res.status(500).send(err.message);
